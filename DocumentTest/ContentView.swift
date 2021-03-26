d9e0853f14ca53a391de9eaef271b25b1af2ac95
importRyan <importRyan@gmail.com>
2021-03-26 13:38:49 -0700

    Undo menu item title does not display

--- DocumentTest/ContentView.swift
+++ DocumentTest/ContentView.swift
@@ -1,22 +1,72 @@
-//
-//  ContentView.swift
-//  DocumentTest
-//
-//  Created by Ryan Ferrell on 3/26/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @Binding var document: DocumentTestDocument
-
+    @Environment(\.undoManager) var undo
+    @StateObject var vm = VM()
+    
+    @State var autoEnables = false
+    
     var body: some View {
-        TextEditor(text: $document.text)
+        VStack(spacing: 25) {
+            HStack {
+                Button("<") { vm.performUndo(undo: undo) }
+                    .disabled(!(undo?.canUndo ?? true))
+                
+                Button("Up") { vm.increment(undo: undo) }
+                Button("Down") { vm.decrement(undo: undo) }
+                
+                Button(">") { vm.performRedo(undo: undo) }
+                    .disabled(!(undo?.canRedo ?? true))
+            }
+            
+            Text(String(vm.count))
+                .font(.title)
+            
+            Text("MenuItemTitle \(vm.title)")
+            
+        }
+        .controlSize(.large)
+        .font(.title3)
+        .frame(width: 400, height:  300)
+        .onAppear { DispatchQueue.main.async { autoEnables = NSApp.menu?.autoenablesItems ?? false } }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(document: .constant(DocumentTestDocument()))
+class VM: ObservableObject {
+    
+    @Published var count = 0
+    @Published var title = ""
+    
+    weak var undo: UndoManager? = nil
+    
+    func increment(undo: UndoManager?) {
+        count += 1
+        
+        undo?.registerUndo(withTarget: self, handler: { (targetSelf) in
+            targetSelf.decrement(undo: undo)
+        })
+        undo?.setActionName("Increment")
+        
+        title = undo?.undoMenuItemTitle ?? "Nil"
+    }
+    
+    func decrement(undo: UndoManager?) {
+        count -= 1
+        
+        undo?.registerUndo(withTarget: self, handler: { (targetSelf) in
+            targetSelf.increment(undo: undo)
+        })
+        undo?.setActionName("Increment")
+        
+        title = undo?.undoMenuItemTitle ?? "Nil"
+    }
+    
+    func performUndo(undo: UndoManager?) {
+        undo?.undo()
+    }
+    
+    func performRedo(undo: UndoManager?) {
+        undo?.redo()
     }
 }
